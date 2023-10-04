07f88a3085a3712ae92eecff2983cd8d331ff882
patofran <patofran69@gmail.com>
2023-10-04 19:43:27 +0200

    mas cambios

--- ud3/ejercicioSoporte/CintaVideo.php
+++ ud3/ejercicioSoporte/CintaVideo.php
@@ -1,3 +1,18 @@
 <?php
+    class CintaVideo extends Soporte{
+        private $duracion;
 
+        public function __construct($titulo, $numero, $precio, $duracion) {
+            parent::__construct($titulo, $numero, $precio);  
+            $this->duracion = $duracion;
+        }
+
+        public function getDduracion(){
+            return $this->duracion;
+        }
+
+        public function muestraResumen(){
+            return "titulo: " . $this->titulo . " /numero: " . $this->numero . " /duracion: " . $this->duracion . " /precio Sin Iva: " . $this->precio;
+        }
+    }
 ?>
--- ud3/ejercicioSoporte/pruebas.php
+++ ud3/ejercicioSoporte/pruebas.php
@@ -15,5 +15,14 @@
         $soporte1->getPrecioConIVA() . " euros";
         $soporte1->muestraResumen();
     ?>
+
+    <?php
+        $miCinta = new CintaVideo("Los cazafantasmas", 23, 3.5, 107);
+        echo "<strong>" . $miCinta->titulo . "</strong>";
+        echo "<br>Precio: " . $miCinta->getPrecio() . " euros";
+        echo "<br>Precio IVA incluido: " .
+        $miCinta->getPrecioConIva() . " euros";
+        $miCinta->muestraResumen();
+    ?>
 </body>
 </html>
