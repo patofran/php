7b3a2206dfacbfe7602c651d1848cb05c17d8dce
patofran <patofran69@gmail.com>
2023-10-04 20:03:59 +0200

    fdsvdsvrfbgwdfds

--- ud3/ejercicioSoporte/pruebas.php
+++ ud3/ejercicioSoporte/pruebas.php
@@ -17,7 +17,7 @@
         $soporte1->getPrecioConIVA() . " euros<br>";
         echo $soporte1->muestraResumen() . "<br>";
 
-        $miCinta = new CintaVideo("Los cazafantasmas", 23, 3.5, 107);
+        $miCinta = new CintaVideo("Los cazafantasmas", 23, 5, 107);
         echo "<strong>" . $miCinta->titulo . "</strong>";
         echo "<br>Precio: " . $miCinta->getPrecio() . " euros";
         echo "<br>Precio IVA incluido: " .
