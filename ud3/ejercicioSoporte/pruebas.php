587a53ac944642a34b06bdfd6ea719eeb26245fc
patofran <patofran69@gmail.com>
2023-10-04 19:52:26 +0200

    Update pruebas.php

--- ud3/ejercicioSoporte/pruebas.php
+++ ud3/ejercicioSoporte/pruebas.php
@@ -8,16 +8,15 @@
 <body>
     <?php
         include "Soporte.php";
+        include "CintaVideo.php";
+
         $soporte1 = new Soporte("Tenet", 22, 3);
         echo "<strong>" . $soporte1->titulo . "</strong>";
         echo "<br>Precio: " . $soporte1->getPrecio() . " euros";
         echo "<br>Precio IVA incluido: " .
         $soporte1->getPrecioConIVA() . " euros";
         $soporte1->muestraResumen();
-    ?>
-
-    <?php
-        include "CintaVideo.php";
+        
         $miCinta = new CintaVideo("Los cazafantasmas", 23, 3.5, 107);
         echo "<strong>" . $miCinta->titulo . "</strong>";
         echo "<br>Precio: " . $miCinta->getPrecio() . " euros";
