91f7b95dbdee6a403d8df59683267b5017c074c2
patofran <patofran69@gmail.com>
2023-10-04 20:17:13 +0200

    Update Dvd.php

--- ud3/ejercicioSoporte/Dvd.php
+++ ud3/ejercicioSoporte/Dvd.php
@@ -1,15 +1,17 @@
 <?php
 
-    private $idiomas;
-    private $formatPantalla;
+    class Dvd{
+        private $idiomas;
+        private $formatPantalla;
 
-    public function __construct($titulo, $numero, $precio, $idiomas, $formatPantalla) {
-        parent::__construct($titulo, $numero, $precio);  
-        $this->idiomas = $idiomas;
-        $this->formatPantalla = $formatPantalla;
-    }
+        public function __construct($titulo, $numero, $precio, $idiomas, $formatPantalla) {
+            parent::__construct($titulo, $numero, $precio);  
+            $this->idiomas = $idiomas;
+            $this->formatPantalla = $formatPantalla;
+        }
 
-    public function muestraResumen(){
-        return "titulo: {$this->titulo}, numero: {$this->numero}, idioma {$this->idioma}, formato de la pantalla: {$this->formatPantalla}, precio Sin Iva: {$this->getPrecio()}";
+        public function muestraResumen(){
+            return "titulo: {$this->titulo}, numero: {$this->numero}, idioma {$this->idioma}, formato de la pantalla: {$this->formatPantalla}, precio Sin Iva: {$this->getPrecio()}";
+        }
     }
 ?>
