7934a96863bb3826ac1563732604609d7a0636da
patofran <patofran69@gmail.com>
2023-10-04 20:16:10 +0200

    Update pruebas.php

--- ud3/ejercicioSoporte/pruebas.php
+++ ud3/ejercicioSoporte/pruebas.php
@@ -9,15 +9,15 @@
     <?php
         include "Soporte.php";
         include "CintaVideo.php";
+        include "Dvd.php";
         
-        
-
-        $miCinta = new CintaVideo("Los cazafantasmas", 23, 3.5, 107);
-        echo "<strong>" . $miCinta->titulo . "</strong>";
-        echo "<br>Precio: " . $miCinta->getPrecio() . " euros";
+        $miDvd = new Dvd("Origen", 24, 15, "es,en,fr",
+        "16:9");
+        echo "<strong>" . $miDvd->titulo . "</strong>";
+        echo "<br>Precio: " . $miDvd->getPrecio() . " euros";
         echo "<br>Precio IVA incluido: " .
-        $miCinta->getPrecioConIva() . " euros <br>";
-        echo $miCinta->muestraResumen() . "<br>";
+        $miDvd->getPrecioConIva() . " euros";
+        $miDvd->muestraResumen();
     ?>
 </body>
 </html>
