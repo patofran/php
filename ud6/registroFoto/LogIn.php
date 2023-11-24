19254c6cb93a99b6a5ec2c82c627edcb249d3539
patofran <patofran69@gmail.com>
2023-11-25 00:50:59 +0100

    mas cosas aun

---
+++ ud6/registroFoto/LogIn.php
@@ -0,0 +1,109 @@
+<?php
+    session_start();
+
+    $opc = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+    try {
+        $conexion = new PDO('mysql:host=localhost;dbname=discografia', 'disco', 'disco', $opc);
+    } catch (PDOException $e) {
+        echo 'Falló la conexión: ' . $e->getMessage();
+    }
+
+    $usuario;
+    $cont;
+    $infoError;
+
+    if (isset($_GET["cont"]) && isset($_GET["usu"])) {
+        $cont = $_GET["cont"];
+        $usu = $_GET["usu"];
+
+        $consulta = $conexion->prepare("SELECT `password` FROM `tabla_usuarios` WHERE `usuario` LIKE ?;");
+        $consulta->execute([$usu]);
+        $exsiste = $consulta->fetch();
+
+        if ($exsiste) {
+            $hashUsuario = $exsiste["password"];
+            if (password_verify($cont, $hashUsuario)) {
+                setcookie("sesionUsuario", $usu, 0, "/");
+                header("Location: index.php");
+                exit;
+            }else {
+                $infoError = "La contraseña no esta bien";
+            }
+        }else {
+            $infoError = "El usuario no existe.";
+        }
+
+    }
+
+    if (isset($_GET["respuesta"])) {
+        $res = $_GET["respuesta"];
+        if ($res == "no") {
+            setcookie("sesionUsuario", "", -1, "/");
+        } 
+    }
+?>                      
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Inicio de sesion</title>
+        <link rel="stylesheet" href="css\style.css" type="text/css">
+    </head>
+    <body>
+        <header id="formulario">
+        
+            <?php 
+
+                if (isset($_GET["respuesta"])) {
+                    $res = $_GET["respuesta"];
+                    if ($res == "si") {
+                        header("Location: index.php");
+                        exit;
+                    }
+                    
+                }
+
+                if (isset($_COOKIE["sesionUsuario"])) {
+                    $nombreCookie = $_COOKIE["sesionUsuario"];
+                    echo '
+                        <h1>Ya se inicio anteriormente como ' . $nombreCookie . '</h1>
+                        <form action="'. $_SERVER["PHP_SELF"] .'" method="get">
+                            <p>¿Deseas entrar con ese usuario?</p>
+                            <button type="submit" name="respuesta" value="si">Sí</button>
+                            <button type="submit" name="respuesta" value="no">No</button>
+                        </form>
+                    ';
+                    
+                    $_SESSION['usuario'] = $nombreCookie;
+                }
+            ?>
+
+
+            <?php 
+                if (isset($_GET["nuevo"])) {
+                    $nuevo = $_GET["nuevo"];
+                    echo "<h1>usuario $nuevo creado con exito!!</h1>";
+                }
+            ?>
+            <h1>Inicio de sesion</h1>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
+                <p>Nombre: </p>
+                <input type="text" id="usu" name="usu" required><br><br>
+                
+                <p>Contraseña: </p>
+                <input type="password" id="cont" name="cont" required><br><br>
+
+                <input type="submit" value="Entrar">
+            </form>
+            
+            <p>
+                <?php if (isset($infoError)) {
+                    echo $infoError;
+                } ?>
+            </p>
+                <p></p>
+            <p>No estas registrado, pincha <a href="registro.php">aqui</a></p>
+        </header>
+    </body>
+</html>
--- ud6/registroFoto/Registro.php
+++ ud6/registroFoto/Registro.php
@@ -1,4 +1,44 @@
 <?php
-    
+    $opc = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+    try {
+        $conexion = new PDO('mysql:host=localhost;dbname=discografia', 'disco', 'disco', $opc);
+    } catch (PDOException $e) {
+        echo 'Falló la conexión: ' . $e->getMessage();
+    }
 
+    $usuario;
+    $cont;
+
+    if (isset($_GET["cont"]) && isset($_GET["usu"])) {
+        $cont = password_hash($_GET["cont"], PASSWORD_DEFAULT);
+        $usuario = $_GET["usu"];
+        $conexion->query("INSERT INTO `tabla_usuarios` (`id`, `usuario`, `password`) VALUES (NULL, '$usuario', '$cont')");
+        header("Location: LogIn.php?nuevo=$usuario");
+    }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css\style.css" type="text/css">
+    <title>Nuevo usuario</title>
+</head>
+<body>
+    <section id="formulario">
+        <h2>Registro de Nuevo Usuario</h2>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
+                <p>Usuario: </p>
+                <input type="text" id="usu" name="usu" required><br><br>
+                
+                <p>Contraseña: </p>
+                <input type="password" id="cont" name="cont" required><br><br>
+
+                <input type="submit" value="Registro">
+        </form>
+
+        <p>Si ya estas registrado, pincha <a href="logIn.php">aqui</a></p>
+    </section>
+</body>
+</html>
