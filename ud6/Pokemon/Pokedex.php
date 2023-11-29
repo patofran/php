6c16a780530d2f65ce0b63a1cb8564090a500406
patofran <patofran69@gmail.com>
2023-11-29 20:14:26 +0100

    Update Pokedex.php

--- ud6/Pokemon/Pokedex.php
+++ ud6/Pokemon/Pokedex.php
@@ -6,6 +6,22 @@
     <title>Pokedex</title>
 </head>
 <body>
-    
+
+<?php
+    $enlace = "https://pokeapi.co/api/v2/pokemon/?limit=151";
+    $json_data = file_get_contents($enlace);
+    $pokemon_list = json_decode($json_data);
+
+    echo "<h2>Lista de los 10 primeros Pokémon:</h2>";
+    echo "<ul>";
+
+    foreach ($pokemon_list->results as $pokemon) {
+        echo "<li>Nombre: " . ucfirst($pokemon->name) . "</li>";
+    }
+
+    echo "</ul>";
+?>
+
+
 </body>
 </html>
