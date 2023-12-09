9239e2c52b6109de84249ef4726437c5f413f941
patofran <patofran69@gmail.com>
2023-12-09 15:32:58 +0100

    cosas pokemon

---
+++ ud6/Pokemon/Trabajo Pokemon/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Pokemon</title>
+	<link rel="stylesheet" type="text/css" href="examen.css">
+</head>
+<body>
+ 
+<header> Mi blog de &nbsp;&nbsp; <img src="img/International_Pokémon_logo.svg.png"></header>
+
+<div></div>
+
+<nav><strong>G1 Kanto &nbsp;&nbsp; G2 Johto &nbsp;&nbsp; G3 Hoenn  &nbsp;&nbsp; G4 Sinnoh  &nbsp;&nbsp; G5 Unova  &nbsp;&nbsp; G6 Kalos  &nbsp;&nbsp; G7 Alola &nbsp;&nbsp; G8 Galar &nbsp;&nbsp; G9 Paldea &nbsp;&nbsp; Búsqueda</strong> </nav>
+
+<div id="iniciales">
+
+</div>
+
+
+<div class="abajo"></div>
+
+<footer> Trabajo &nbsp;<strong> Desarrollo Web en Entorno Servidor </strong>&nbsp; 2023/2024 IES Serra Perenxisa.</footer>
+
+</body>
+</html>
