e680522dd4e144c55e98cbfc61e92e03966ae1ad
patofran <patofran69@gmail.com>
2023-12-10 11:10:08 +0100

    otra mas

--- ud6/Pokemon/Trabajo Pokemon/sinnoh.php
+++ ud6/Pokemon/Trabajo Pokemon/sinnoh.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Pokemon G3 Hoenn</title>
+	<title>Pokemon G4 Sinnoh</title>
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
