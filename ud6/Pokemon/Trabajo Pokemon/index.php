819e5aa7f0c8932beeb7e570b346498607b0fa3e
patofran <patofran69@gmail.com>
2023-12-10 15:12:54 +0100

    tipos

--- ud6/Pokemon/Trabajo Pokemon/index.php
+++ ud6/Pokemon/Trabajo Pokemon/index.php
@@ -4,40 +4,74 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Pokemon</title>
-	<link rel="stylesheet" type="text/css" href="examen.css">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
  
-	<header> Mi blog de &nbsp;&nbsp; <img src="img/International_Pokémon_logo.svg.png"></header>
+<header> Mi blog de &nbsp;&nbsp; <img src="img/International_Pokémon_logo.svg.png"></header>
 
-	<div></div>
+<nav><strong>G1 Kanto &nbsp;&nbsp; G2 Johto &nbsp;&nbsp; G3 Hoenn  &nbsp;&nbsp; G4 Sinnoh  &nbsp;&nbsp; G5 Unova  &nbsp;&nbsp; G6 Kalos  &nbsp;&nbsp; G7 Alola &nbsp;&nbsp; G8 Galar &nbsp;&nbsp; G9 Paldea &nbsp;&nbsp; Búsqueda</strong> </nav>
 
-	<nav><strong>G1 Kanto &nbsp;&nbsp; G2 Johto &nbsp;&nbsp; G3 Hoenn  &nbsp;&nbsp; G4 Sinnoh  &nbsp;&nbsp; G5 Unova  &nbsp;&nbsp; G6 Kalos  &nbsp;&nbsp; G7 Alola &nbsp;&nbsp; G8 Galar &nbsp;&nbsp; G9 Paldea &nbsp;&nbsp; Búsqueda</strong> </nav>
+<section id="contenido">
+	<br>
+	<form action="<?php $_SERVER['PHP_SELF'] ?>" method="GET">
+		<label for="nombre">Buscar por nombre:</label>
+		<input type="text" id="nombre" name="nombre" placeholder="Nombre del Pokémon">
+		<br> <br>
+		<label for="tipo">Buscar por tipo:</label>
+		<select id="tipo" name="tipo">
+			<option value="">Selecciona un tipo</option>
+			<option value="normal">Normal</option>
+			<option value="fighting">Lucha</option>
+			<option value="flying">Volador</option>
+			<option value="poison">veneno</option>
+			<option value="ground">Tierra</option>
+			<option value="rock">Roca</option>
+			<option value="bug">Bicho</option>
+			<option value="ghost">Fantasma</option>
+			<option value="steel">Acero</option>
+			<option value="fire">Fuego</option>
+			<option value="grass">Planta</option>
+			<option value="water">Agua</option>
+			<option value="electric">Electrico</option>
+			<option value="psychic">Psiquico</option>
+			<option value="ice">Hielo</option>
+			<option value="dragon">Dragon</option>
+			<option value="dark">Siniestro</option>
+			<option value="fairy">Hada</option>
+		</select>
 
-	<div id="iniciales">
-		<?php
-				$ch = curl_init(); 
-				$url = "https://pokeapi.co/api/v2/pokemon/?limit=10";
-				curl_setopt($ch, CURLOPT_URL, $url);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-				$json_data = curl_exec($ch);
-				curl_close($ch);
+		<button type="submit">Buscar</button>
+	</form>
 
-				$arrayPokemon = json_decode($json_data);
+	<?php
+		if ($_SERVER["REQUEST_METHOD"] == "GET") {
+			if (isset($_GET["tipo"])) {
+				$tipo = isset($_GET["tipo"]) ? $_GET["tipo"] : "";
 
-				foreach ($arrayPokemon->results as $pokemon) {
-					$ch = curl_init($pokemon->url);
-					curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-					$datosPokemon = json_decode(curl_exec($ch));
-					curl_close($ch);
-					echo "<div id='ult_entradas'><img src='" . $datosPokemon->sprites->front_default . "' alt=''><p>" . ucfirst($pokemon->name) . " </p></div>";
+				$url = "https://pokeapi.co/api/v2/type/$tipo";
+				$json_data = file_get_contents($url);
+
+				if ($json_data !== false) {
+					$tipoData = json_decode($json_data);
+
+					foreach ($tipoData->pokemon as $pokemon) {
+						$ch = curl_init($pokemon->pokemon->url);
+						curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+						$datosPokemon = json_decode(curl_exec($ch));
+						curl_close($ch);
+
+						echo "<div id='ult_entradas'><img src='" . $datosPokemon->sprites->front_default . "' alt=''><p>" . ucfirst($pokemon->pokemon->name) . "</p></div>";
+					}
+				} else {
+					echo "Error al decodificar los datos JSON para el tipo $tipo.";
 				}
-		?>
-	</div>
+			}
+		}
+	?>
+</section>
 
-	<div class="abajo"></div>
-
-	<footer> Trabajo &nbsp;<strong> Desarrollo Web en Entorno Servidor </strong>&nbsp; 2023/2024 IES Serra Perenxisa.</footer>
+<footer> Trabajo &nbsp;<strong> Desarrollo Web en Entorno Servidor </strong>&nbsp; 2023/2024 IES Serra Perenxisa.</footer>
 
 </body>
 </html>
