2ff03c5b5f8a9b5bf3a4503d93163e947bbaa3c0
patofran <patofran69@gmail.com>
2023-12-11 14:57:01 +0100

    asfdsafdsgfddf

--- ud6/registroFoto/Perfil.php
+++ ud6/registroFoto/Perfil.php
@@ -2,7 +2,18 @@
     require_once('Conexion.inc.php');
     $conexion = Conexion::obtenerConexion();
 
-    if (isset($_COOKIE[""])) {
-        # code...
+    if (isset($_COOKIE["sesionUsuario"])) {
+        $nombre = $_COOKIE["sesionUsuario"];
+
+        $consulta = $conexion->prepare("SELECT * FROM `usuarios` WHERE `nombre` LIKE ?;");
+        $consulta->execute([$nombre]);
+        $usuario = $consulta->fetch();
+        
+        if ($usuario) {
+            echo $nombre . " <img src='" . $usuario["urlFoto"] . "' alt='' style='width: 72px; height: 96px;'>";
+            echo "<br><br><br><br><br><br>Tu nombre se perfil es: " . $usuario["nombre"] . "<br> Tu foto de perfil es: <br>" . " <img src='" . $usuario["urlFoto"] . "' alt='' style='width: 360px; height: 480px;'>";
+            echo "<br>Y tu id es el: " . $usuario["idUsuario"];
+        }
+        
     }
 ?>
